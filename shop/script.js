d27d57eb461892748eca362e4c4842d2f4dd293e
Mataversa <143892014+Mataversa@users.noreply.github.com>
2024-08-16 17:09:27 +0200

    Add files via upload

---
+++ shop/script.js
@@ -0,0 +1,4 @@
+document.addEventListener("DOMContentLoaded", function () {
+  // Tutaj można dodać skrypty JavaScript, np. obsługę przycisków, animacje itp.
+  console.log("Strona załadowana");
+});
