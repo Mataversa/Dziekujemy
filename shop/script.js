2771dd2eaac35bbb1fc449fce5c1f1e9e91fc5ea
Mataversa <143892014+Mataversa@users.noreply.github.com>
2024-08-17 16:48:30 +0200

    Delete shop directory

--- shop/script.js
+++
@@ -1,4 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-  // Tutaj można dodać skrypty JavaScript, np. obsługę przycisków, animacje itp.
-  console.log("Strona załadowana");
-});
